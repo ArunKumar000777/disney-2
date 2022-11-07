73a0f79b13f7e3e813c11a358681dec98b4e1e69
ArunKumar000777 <arunashok1020@gmail.com>
2022-11-07 22:37:34 +0530

    change app js

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Detail from "./components/Detail";
 import Login from "./components/Login";
-import {selectUserName} from './features/user/userSlice'
+import { selectUserName } from "./features/user/userSlice";
 import { useSelector } from "react-redux";
 
-
 function App() {
-const userName = useSelector(selectUserName)
+    const userName = useSelector(selectUserName);
     return (
         <div className="App">
             <Router>
                 <Header />
                 <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/" element={!userName ? <Navigate to={'/login'}/> : <Home />} />
+                    <Route path="/" element={userName ? <Navigate to={"/home"} /> : <Navigate to={"/login"} />} />
+                    <Route path="/home" element={userName ? <Home /> : <Navigate to="../login" />} />
+                    <Route path="/login" element={userName ? <Navigate to="../home" /> : <Login />} />
                     <Route path="/detail/:id" element={<Detail />} />
                 </Routes>
             </Router>
